0402cb84ee0efae0cfd51bed6cf6b63cd21dce68
mariakhachatryann <mariakhachatryan23@gmail.com>
2024-09-06 12:17:55 +0400

    fetch with axios

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+
 class ProductController extends Controller
 {
     public function index(Request $request)
@@ -15,13 +15,21 @@ class ProductController extends Controller
             foreach ($request->input('properties') as $propertyName => $propertyValues) {
                 $query->whereHas('properties', function ($q) use ($propertyName, $propertyValues) {
                     $q->where('property_name', $propertyName)
-                        ->whereIn('property_value', $propertyValues);
+                    ->whereIn('property_value', $propertyValues);
                 });
             }
         }
 
         $products = $query->get();
 
-        return response()->json($products);
+        return response()->json([
+            'data' => $products,
+            'meta' => [
+            'current_page' => 1, // adjust as necessary
+            'last_page' => 1, // adjust as necessary
+            'prev_page_url' => null, // adjust as necessary
+            'next_page_url' => null, // adjust as necessary
+            ]
+        ]);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,45 +6,17 @@ use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use GuzzleHttp\Client;
 use App\Models\ProductProperty;
-use Illuminate\Support\Collection;
 
 class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        $client = new Client();
-
-        $queryParams = $request->has('properties') ? [
-            'properties' => $request->input('properties')
-        ] : [];
-
-        $response = $client->request('GET', 'http://products-catalog.loc/api/products', [
-            'query' => $queryParams
-        ]);
-
-        $products = json_decode($response->getBody()->getContents(), true);
-
-        $productData = collect($products ?? []);
-
-        $currentPage = LengthAwarePaginator::resolveCurrentPage();
-        $perPage = 40;
-        $currentItems = $productData->slice(($currentPage - 1) * $perPage, $perPage)->all();
-
-        $paginatedProducts = new LengthAwarePaginator(
-            $currentItems,
-            $productData->count(),
-            $perPage,
-            $currentPage,
-            ['path' => $request->url(), 'query' => $request->query()]
-        );
-
         $properties = ProductProperty::select('property_name', 'property_value')
             ->distinct()
             ->get()
             ->groupBy('property_name');
 
         return view('products.index', [
-            'products' => $paginatedProducts,
             'properties' => $properties,
             'request' => $request
         ]);
